7af8d4fae919e05729be0468bc6cdbede9d4c707
huguoyong <humanate@qq.com>
2019-10-22 21:20:36 +0800

    #comment add some branch test

--- src/test/java/com/hu/utils/NumberUtilsTest.java
+++ src/test/java/com/hu/utils/NumberUtilsTest.java
@@ -7,38 +7,147 @@ import org.junit.Test;
 public class NumberUtilsTest {
 
   @Test
+  public void should_return_true_new_numberUtils() {
+    //given
+    //when
+    //then
+    assertThat(new NumberUtils()).isInstanceOf(NumberUtils.class);
+  }
+
+  @Test
   public void should_return_false_when_str_is_null() {
     //given
     //when
     //then
     assertThat(NumberUtils.isCreatable(null)).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_blank() {
+    //given
+    //when
+    //then
     assertThat(NumberUtils.isCreatable("")).isEqualTo(false);
   }
 
   @Test
-  public void should_return_true_when_str_is_hexadecimal_marked_with_0x_or_0X_qualifier() {
+  public void should_return_true_when_str_is_hexadecimal_marked_with_0x_qualifier() {
     //given
-    String str_0x = "0x123";
-    String str_0X = "0X123";
+    String str = "0x123";
     //when
-    boolean result_0X = NumberUtils.isCreatable(str_0X);
-    boolean result_0x = NumberUtils.isCreatable(str_0x);
+    boolean result = NumberUtils.isCreatable(str);
     //then
-    assertThat(result_0X).isEqualTo(true);
-    assertThat(result_0x).isEqualTo(true);
+    assertThat(result).isEqualTo(true);
   }
 
   @Test
-  public void should_return_false_when_str_is_not_hexadecimal_marked_with_0x_or_0X_qualifier() {
+  public void should_return_true_when_str_is_hexadecimal_marked_with_0X_qualifier() {
     //given
-    String str_0X = "0xGH";
-    String str_0x = "0xHU";
+    String str = "0X123";
     //when
-    boolean result_0X = NumberUtils.isCreatable(str_0X);
-    boolean result_0x = NumberUtils.isCreatable(str_0x);
+    boolean result = NumberUtils.isCreatable(str);
     //then
-    assertThat(result_0x).isEqualTo(false);
-    assertThat(result_0X).isEqualTo(false);
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_not_hexadecimal_marked_with_0x_qualifier() {
+    //given
+    String str = "0xGH";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_not_hexadecimal_marked_with_0X_qualifier() {
+    //given
+    String str = "0xGH";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_sign_up_plus() {
+    //given
+    String str = "+123";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_sign_up_minus() {
+    //given
+    String str = "-123";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_less_than_a() {
+    //given
+    String str = "0X.";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_less_than_A() {
+    //given
+    String str = "0X@";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_less_than_0() {
+    //given
+    String str = "0X/";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_more_than_9() {
+    //given
+    String str = "0X:";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_more_than_f() {
+    //given
+    String str = "0Xg";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_more_than_F() {
+    //given
+    String str = "0XG";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
   }
 
   @Test
@@ -46,9 +155,9 @@ public class NumberUtilsTest {
     //given
     String str = "0x";
     //when
-    boolean result_0x = NumberUtils.isCreatable(str);
+    boolean result = NumberUtils.isCreatable(str);
     //then
-    assertThat(result_0x).isEqualTo(false);
+    assertThat(result).isEqualTo(false);
   }
 
   @Test
@@ -62,9 +171,20 @@ public class NumberUtilsTest {
   }
 
   @Test
-  public void should_return_false_when_str_is_Non_hexadecimal_beginning_with_a_leading_zero() {
+  public void should_return_false_when_str_is_Non_hexadecimal_beginning_with_a_leading_zero_less_than_0() {
     //given
-    String str = "09";
+    String str = "0/";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+
+  @Test
+  public void should_return_false_when_str_is_Non_hexadecimal_beginning_with_a_leading_zero_more_than_7() {
+    //given
+    String str = "08";
     //when
     boolean result = NumberUtils.isCreatable(str);
     //then
@@ -79,6 +199,16 @@ public class NumberUtilsTest {
     boolean result = NumberUtils.isCreatable(str);
     //then
     assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_contains_point_exp() {
+    //given
+    String str = "501E2.5";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
   }
 
   @Test
@@ -132,9 +262,29 @@ public class NumberUtilsTest {
   }
 
   @Test
-  public void should_return_true_when_str_is_plus_after_e() {
+  public void should_return_false_when_str_is_minus_not_after_e() {
     //given
-    String str = "12e+12";
+    String str = "12e2-12";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_plus_e_e() {
+    //given
+    String str = "-12e";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_minus_after_e() {
+    //given
+    String str = "12e-12";
     //when
     boolean result = NumberUtils.isCreatable(str);
     //then
@@ -160,10 +310,31 @@ public class NumberUtilsTest {
     //then
     assertThat(result).isEqualTo(false);
   }
+
+  @Test
+  public void should_return_false_when_str_is_not_end_with_e() {
+    //given
+    String str = "123e";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
   @Test
   public void should_return_false_when_str_is_marked_with_a_type_qualifier_and_has_two_point() {
     //given
     String str = "12.3.";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_marked_with_a_type_qualifier_and_has_point_epx() {
+    //given
+    String str = "12E3.";
     //when
     boolean result = NumberUtils.isCreatable(str);
     //then
@@ -181,13 +352,84 @@ public class NumberUtilsTest {
   }
 
   @Test
-  public void should_return_true_when_str_is_marked_with_a_type_qualifier_and_front_is_digit() {
+  public void should_return_true_when_str_is_marked_with_a_type_qualifier_and_end_with_d() {
     //given
     String str = "12d";
     //when
     boolean result = NumberUtils.isCreatable(str);
     //then
     assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_marked_with_a_type_qualifier_and_end_with_D() {
+    //given
+    String str = "12D";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_marked_with_a_type_qualifier_and_end_with_f() {
+    //given
+    String str = "1.2f";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_marked_with_a_type_qualifier_and_end_with_F() {
+    //given
+    String str = "12F";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_marked_with_a_type_qualifier_and_end_with_L() {
+    //given
+    String str = "12L";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(true);
+  }
+
+  @Test
+  public void should_return_true_when_str_is_marked_with_a_type_qualifier_and_end_with_l() {
+    //given
+    String str = "12l";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(true);
+  }
+
+
+  @Test
+  public void should_return_false_when_str_is_marked_with_a_type_qualifier_and_end_with_point_L() {
+    //given
+    String str = "1.2L";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_marked_with_a_type_qualifier_and_end_with_exp_l() {
+    //given
+    String str = "1E2L";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
   }
 
   @Test
@@ -201,9 +443,19 @@ public class NumberUtilsTest {
   }
 
   @Test
-  public void should_return_false_when_str_is_end_with_e() {
+  public void should_return_false_when_str_is_plus() {
     //given
-    String str = "123E+E";
+    String str = "+";
+    //when
+    boolean result = NumberUtils.isCreatable(str);
+    //then
+    assertThat(result).isEqualTo(false);
+  }
+
+  @Test
+  public void should_return_false_when_str_is_minus() {
+    //given
+    String str = "1-";
     //when
     boolean result = NumberUtils.isCreatable(str);
     //then
